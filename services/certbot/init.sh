3cd0b501ee9922f2c0d003623c63d887d0355b69
Zang MingJie <zealot0630@gmail.com>
2022-09-12 19:00:47 +0800

    Add certbot group

--- services/certbot/init.sh
+++ services/certbot/init.sh
@@ -1,5 +1,28 @@
 #! /bin/sh
 
-BASEDIR=$(dirname "$0")
+BASEDIR=$(realpath $(dirname "$0"))
+CERTBOT_DIR=/etc/letsencrypt
+
+groupadd -g 28401 certbot
+
+mkdir --verbose -p ${CERTBOT_DIR}
+chgrp --changes certbot ${CERTBOT_DIR}
+chmod --changes g+srx ${CERTBOT_DIR}
+
+mkdir -p ${CERTBOT_DIR}/live ${CERTBOT_DIR}/archive
+chgrp --changes certbot ${CERTBOT_DIR}/live ${CERTBOT_DIR}/archive
+chmod --changes g+srx ${CERTBOT_DIR}/live ${CERTBOT_DIR}/archive
+
+for i in ${CERTBOT_DIR}/live/* ${CERTBOT_DIR}/archive/* ; do
+    if [ -d $i ] ; then
+        chgrp --changes certbot ${i}
+        chmod --changes g+srx ${i}
+
+        for j in $i/* ; do
+            chgrp --changes certbot ${j}
+            chmod --changes g+r ${j}
+        done
+    fi
+done
 
 certbot certonly --config ${BASEDIR}/cli.ini --manual --preferred-challenge=dns --manual-auth-hook="${BASEDIR}/acme-nsupdate.sh" --manual-cleanup-hook="${BASEDIR}/acme-clean.sh" --post-hook="${BASEDIR}/post-hook-update-owner.sh" --domain $(hostname -f)
--- services/certbot/post-hook-update-owner.sh
+++ services/certbot/post-hook-update-owner.sh
@@ -2,10 +2,9 @@
 
 if [ -z "$RENEWED_LINEAGE" ]
 then
-	RENEWED_LINEAGE="/etc/letsencrypt/live/$(hostname -f)/privkey.pem"
-	echo Using file "$RENEWED_LINEAGE"
+	RENEWED_LINEAGE="/etc/letsencrypt/live/$(hostname -f)"
+	echo Using dir "$RENEWED_LINEAGE"
 fi
 
-USER=root
-
-chown --dereference "${USER}" "${RENEWED_LINEAGE}/privkey.pem"
+chgrp --changes certbot "${RENEWED_LINEAGE}/privkey.pem"
+chmod --changes g+r "${RENEWED_LINEAGE}/privkey.pem"
